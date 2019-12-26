841e963387dd841b5c09ab13fae742e5a635ba14
Matuesz <bieganski.m@wp.pl>
2019-12-26 22:06:20 +0100

    thinking out loud about SSA

--- src/Backend.hs
+++ src/Backend.hs
@@ -77,3 +77,42 @@ prolog fname = ["source_filename = " ++ "\"" ++ fname ++ "\"",
           "target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"",
           "target triple = \"x86_64-pc-linux-gnu\"\n",
           "@.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1\n"]
+
+epilog = ["declare i32 @printf(i8*, ...) #1",
+          "attributes #0 = { noinline nounwind optnone uwtable \"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" \"no-jump-tables\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" \"use-soft-float\"=\"false\" }",
+          "attributes #1 = { \"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" \"use-soft-float\"=\"false\" }",
+          "!llvm.module.flags = !{!0}",
+          "!llvm.ident = !{!1}\n",
+          "!0 = !{i32 1, !\"wchar_size\", i32 4}",
+          "!1 = !{!\"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)\"}"]
+
+main_begin = ["define i32 @main() #0 {"]
+main_end = ["ret i32 0", "}"]
+
+buildText :: [T.Text] -> T.Text
+buildText ts = T.intercalate (T.pack "\n") ts
+
+buildLines :: [String] -> T.Text
+buildLines xs = T.intercalate (T.pack "\n") (map T.pack xs) 
+
+typeToIRSize :: Type -> String
+typeToIRSize t = case t of
+  Int -> "i32"
+  Str -> "i8*"
+  Bool -> "i8"
+  Void -> error "cannot typeToIRSIze for Void"
+  Fun _ _ -> error "cannot typeToIRSIze for Fun"
+
+ 
+buildCommaString :: [String] -> String 
+buildCommaString lst = T.unpack $ T.intercalate (T.pack ",") $ map T.pack lst
+
+buildFunctionIR :: String -> Type -> T.Text -> T.Text
+buildFunctionIR funName (Fun ret args)  content = buildText
+  [T.pack begin,
+   content,
+   T.pack "}"] where begin = "define " ++ (typeToIRSize ret) ++ "@" ++ funName ++ "("
+                       ++ (buildCommaString (map typeToIRSize args)) ++ ")" ++ "#0 {"
+
+
+
